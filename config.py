ad12a4197784448b4518a1a3f8603bdca03c6175
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2025-06-14 20:13:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Gunjan890/Iihifbif",
+    "https://github.com/Gunjan890/Alpha",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
